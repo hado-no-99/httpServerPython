2af94378437789b91d7b62baa8bd0d98842bd9a0
nehalali110 <nehalalikhan110@gmail.com>
2025-09-29 01:57:13 +0500

    Added POST METHOD TO add new quotes

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 
+quotes = [
+    {"id": 1, "text": "Be the change you wish to see in the world", "author": "Gandhi"},
+    {"id": 2, "text": "The only way to do great work is to love what you do", "author": "Steve Jobs"}
+]
+
 class SimpleRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/" or self.path == "/home":
@@ -38,6 +43,46 @@ class SimpleRequestHandler(BaseHTTPRequestHandler):
             message = {"message" : "This is about us"}
             self.wfile.write(json.dumps(message).encode())
 
+        else:
+            self.send_response(404)
+            message = "Invalid request"
+            self.send_header("Content-Type", "text/plain")
+            self.send_header("Content-Length", int(len(message)))
+            self.end_headers()
+            self.wfile.write(message.encode())
+
+    
+    def do_POST(self):
+        if self.path == "/quotes":
+            try:
+                input_size = int(self.headers.get('Content-Length', 0))
+                if input_size == 0:
+                    raise Exception("Data Required in POST Request")
+
+                raw_input = self.rfile.read(input_size)
+                json_input = raw_input.decode()
+                dict_input = json.loads(json_input)
+
+                if not "text" in dict_input or not "author" in dict_input:
+                    raise Exception("Invalid Data") 
+
+                quotes.append(dict_input)
+
+                message_output = json.dumps({"status" : "success", "message" : "created"})
+                self.send_response(201)
+                self.send_header("Content-Type", "application/json")
+                self.send_header("Content-Length", str(len(message_output)))
+                self.end_headers()
+                self.wfile.write(message_output.encode())
+
+            except Exception as e:
+                self.send_response(400)
+                message_output = json.dumps({"status" : "error", "message" : str(e)})
+                self.send_header("Content-Type", "application/json")
+                self.send_header("Content-Length", str(len(message_output)))
+                self.end_headers()
+                self.wfile.write(message_output.encode())
+            
         
 
 
